fd226900d06fdf8583607fe524590aa7dc9c608d
artem <Artem-lazariev>
2026-01-17 10:50:31 +0200

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Happy developing ✨')
+console.log(typeof (5+5+"5"))
+let email = "my@gmail.com"
+console.log(email.includes("@"))
+console.log(email.length)
+let my = "my "
+let name = "name "
+let is = "is "
+let victor = "victor "
+let fullname = my + name + is + victor
+console.log(fullname)
+
+user = {
+    name: " Олександро!",
+    cost:300
+}
+console.log("thanks " + user.name + " pay " + user.cost + " please!")
